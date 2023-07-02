e04d964658023667fa1a0f16edd047b193ddfe2c
MiltonOscarFlores <miltonoscarflores@gmail.com>
2023-07-01 22:29:03 -0300

    commit

---
+++ U04 - js practica 2/02/app.js
@@ -0,0 +1,21 @@
+/*
+2. Realizar un programa que una vez que solicita dos números por
+teclado, los pasa a una función donde se calcula la suma de éstos.
+La función devolverá dicho resultado, pero en el caso de que salga negativo,
+deberá cambiarle el signo antes de devolverlo. Imprimir el
+resultado en la función principal. 
+
+*/
+
+function suma(n1, n2) {
+    return n1 + n2
+}
+
+function main() {
+    let n1 = Number(prompt("Ingrese un numero"))
+    let n2 = Number(prompt("Ingrese un numero"))
+    alert(suma(n1, n2))
+
+}
+
+main()
